b2da5a4eb68fa2fe61fac1af8ab30b58ff7824e7
Karol Horowski <karol.hor@gmail.com>
2022-11-09 19:49:23 +0100

    update poetry installation

--- python/install.sh
+++ python/install.sh
@@ -5,4 +5,7 @@ then
     echo "  Installing Poetry (Python) for you."
 
     curl -sSL https://install.python-poetry.org | python3 -
+    
+    mkdir -p $HOME/.zfunc
+    poetry completions zsh > ~/.zfunc/_poetry
 fi  
