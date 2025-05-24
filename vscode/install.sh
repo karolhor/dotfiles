5a312ae3030c7cb86a6dad8cc80e4ea62fc37b8c
Karol Horowski <karol.hor@gmail.com>
2025-05-24 12:32:34 +0200

    remove visual studio code sync

--- vscode/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if command -v code >/dev/null; then
-    echo "  Installing VScode extensions for you."
-	
-    while read -r module; do
-		code --install-extension "$module" > /dev/null || true
-	done <"$DOTFILES/vscode/extensions.txt"
-fi
